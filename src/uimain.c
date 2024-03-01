d8fe385a67d6e66075f9923b34aa48fcc15167c0
Naramirez38 <naramirez38@miners.utep.edu>
2024-02-29 18:26:02 -0700

    Idk why my I compile and get a segmentation fault... think its my pointer

--- src/uimain.c
+++ src/uimain.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
-#define maxin 1000
-int main()
-{
-  char *arrpoint;
-  char str[maxin];
-  char *arrpoint = &str[0]// pointer str[0]
-  puts("Hi welcome! just type some input for now \n" );
-  //Put 1 inside while-loop to ensure no termination
-  while(1){
-    printf("\n>");
-    fgets(str,maxin, stdin);
-    printf("you input: %s", str);
-    
-  }
-}
 
+#define maxin 1000
+
+int main()
+
+{
+
+  char *arrpoint;
+
+  char str[maxin];
+ 
+  puts("Hi welcome! just type some input for now \n" );
+  
+  //  *arrpoint = &str[0];//pointer str[0]
+  //Put 1 inside while-loop to ensure no termination
+
+  while(1){
+
+    printf("\n>");
+
+    fgets(str,maxin, stdin);
+
+    printf("you input: %s", str);
+  }
+
+}
